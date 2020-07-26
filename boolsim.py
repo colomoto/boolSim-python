83b9afc6b0fc6d271ae1e8f34d17d5580a7dbff1
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-07-27 00:10:21 +0200

    implement reachable method (fix #2)

--- boolsim.py
+++ boolsim.py
@@ -18,7 +18,7 @@ from colomoto.minibn import BooleanNetwork
 from colomoto.types import *
 from colomoto_jupyter import import_colomoto_tool
 
-def execute(model, update_mode, output):
+def execute(model, update_mode, output, init=None):
     if isinstance(model, BooleanNetwork):
         model = model.to_biolqm()
 
@@ -43,9 +43,11 @@ def execute(model, update_mode, output):
     args = ["boolSim", "-f", model,
                 "-p", update_mode,
                 "-o", os.path.join(output, "out")]
+    if init:
+        args += ["-i", init]
     subprocess.check_call(args)
 
-def parse_attractor(filename):
+def parse_states(filename):
     with open(filename) as fp:
         fp.readline() # skip header
         df = pd.read_fwf(fp, index_col=0, header=None)
@@ -79,8 +81,37 @@ def attractors(bn, update_mode="asynchronous"):
     output = tempfile.mkdtemp(prefix="BoolSim-")
     try:
         execute(bn, update_mode, output)
-        return [parse_attractor(os.path.join(output, out))\
+        return [parse_states(os.path.join(output, out))\
                     for out in os.listdir(output) if out.startswith("out_")]
     finally:
         shutil.rmtree(output)
 
+def reachable(bn, init, update_mode="asynchronous"):
+    """
+    Compute the reachable states within the given Boolean network `bn` from the
+    initial state(s) `init`, using `update_mode` (either ``"asynchronous"`` or
+    ``"synchronous"``).
+
+    `bn` can be either:
+        - ``colomoto.minibn.BooleanNetwork``, ``biolqm``, or ``ginsim`` object
+        - filename in SBML-qual or boolSim format.
+
+    `init` can be either a `dict`, ``colomoto.types.State``,
+        ``colomoto.types.Hypercube``, or
+        ``colomoto.types.HypercubeCollection``.
+
+    Returns a ``colomoto.types.State``,
+        ``colomoto.types.Hypercube``, or
+        ``colomoto.types.HypercubeCollection``, depending on the result.
+    """
+    if not hasattr(init, "as_dataframe"):
+        init = State(init)
+    init = init.as_dataframe().replace("*", 2)
+    wd = tempfile.mkdtemp(prefix="BoolSim-")
+    try:
+        initfile = os.path.join(wd, "initial_states.txt")
+        init.T.to_csv(initfile, sep="\t")
+        execute(bn, update_mode, wd, init=initfile)
+        return parse_states(os.path.join(wd, "out.txt"))
+    finally:
+        shutil.rmtree(wd)
